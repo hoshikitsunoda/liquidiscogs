6f4fd276e07288979cb8f0a745b2e1ea20bb5d1e
Hosh'ki Tsunoda <hoshki@Hoshkis-MacBook-Pro.local>
2019-01-24 14:47:23 -0800

    Install and setup gulp-cache #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const useref = require('gulp-useref');
-const uglify = require('gulp-uglify');
-const gulpIf = require('gulp-if');
-const cssnano = require('gulp-cssnano');
-const imagemin = require('gulp-imagemin');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+const useref = require('gulp-useref')
+const uglify = require('gulp-uglify')
+const gulpIf = require('gulp-if')
+const cssnano = require('gulp-cssnano')
+const imagemin = require('gulp-imagemin')
+const cache = require('gulp-cache')
 
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.stream());
-});
+        .pipe(browserSync.stream())
+})
 
 gulp.task('browserSync', function () {
     browserSync.init({
@@ -28,17 +29,24 @@ gulp.task('useref', function () {
         .pipe(gulpIf('*.js', uglify()))
         .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('dist'))
-});
+})
 
 gulp.task('images', function () {
     return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
-        .pipe(imagemin())
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
         .pipe(gulp.dest('dist/images'))
-});
+})
+
+gulp.task('fonts', function () {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+})
 
 gulp.task('watch', function () {
-    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
-    gulp.watch('app/scss/**/*.scss', gulp.series('browserSync'));
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'))
+    gulp.watch('app/scss/**/*.scss', gulp.series('browserSync'))
+    gulp.watch('app/*.html', browserSync.reload)
+    gulp.watch('app/js/**/*.js', browserSync.reload)
 })
