541e4bbffa5574f57fb61c3c3e86e58db26e789f
Hosh'ki Tsunoda <hoshki@Hoshkis-MacBook-Pro.local>
2019-01-24 13:09:24 -0800

    Install and setup browserSync #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
 
 gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('app/scss/**/*.scss', gulp.series('browserSync'));
 })
